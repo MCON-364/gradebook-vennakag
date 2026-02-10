82439ef041b89f514bc252bd8aae701ef0081fe1
Vivian Ennakag <vennakag@student.touro.edu>
2026-02-10 00:43:14 -0500

    Add Grade Book Methods

--- src/main/java/edu/course/gradebook/Gradebook.java
+++ src/main/java/edu/course/gradebook/Gradebook.java
@@ -13,27 +13,77 @@ public class Gradebook {
     }
 
     public boolean addStudent(String name) {
-        throw new UnsupportedOperationException();
+        boolean added = false;
+        if(!gradesByStudent.containsKey(name)) {
+            gradesByStudent.put(name, new ArrayList<>());
+            added = true;
+        }
+        return added;
     }
 
     public boolean addGrade(String name, int grade) {
-        throw new UnsupportedOperationException();
+        boolean added = false;
+        if(gradesByStudent.containsKey(name)) {
+            gradesByStudent.get(name).add(grade);
+            added = true;
+        }
+        return added;
     }
 
     public boolean removeStudent(String name) {
-        throw new UnsupportedOperationException();
+        boolean removed = false;
+        if(gradesByStudent.containsKey(name)) {
+            gradesByStudent.get(name).remove(0);
+            removed = true;
+        }
+        return removed;
     }
 
     public Optional<Double> averageFor(String name) {
-        throw new UnsupportedOperationException();
+        if(gradesByStudent.containsKey(name)) {
+            var grades = gradesByStudent.get(name);
+            var average = 0.0;
+            for(int g: grades){
+                average += g;
+            }
+            average = average/grades.size();
+            return Optional.of(average);
+        }
+        return Optional.empty();
     }
 
     public Optional<String> letterGradeFor(String name) {
-        throw new UnsupportedOperationException();
+        if(gradesByStudent.containsKey(name)&&!gradesByStudent.get(name).isEmpty()){
+            var average = averageFor(name);
+            var letterGrade = "";
+            if(average.get()>89){
+                letterGrade = "A";
+            }else if(average.get()>79){
+                letterGrade = "B";
+            }else if(average.get()>69){
+                letterGrade = "C";
+            }else if(average.get()>59){
+                letterGrade = "D";
+            }else{
+                letterGrade = "F";
+            }
+            return Optional.of(letterGrade);
+        }
+        return Optional.empty();
     }
 
     public Optional<Double> classAverage() {
-        throw new UnsupportedOperationException();
+        if(gradesByStudent.isEmpty()) return Optional.empty();
+        var students = gradesByStudent.keySet().iterator();
+        var average = 0.0;
+        while(students.hasNext()){
+            var current = students.next();
+            if(averageFor(current).isPresent()){
+                average += averageFor(current).get();
+            }
+        }
+        average = average / gradesByStudent.size();
+        return Optional.of(average);
     }
 
     public boolean undo() {
